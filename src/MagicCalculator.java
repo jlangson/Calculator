ff3a815f51bf970e154d7e968da06a7d258fa139
Jared Langson <langson.jared@gmail.com>
2020-11-16 21:24:03 -0500

    Finished MagicCalculator

--- src/MagicCalculator.java
+++ src/MagicCalculator.java
@@ -1,3 +1,42 @@
+import java.lang.Math;
+
 public class MagicCalculator extends Calculator{
+    //find the square root
+    //mathematically clunky, but good enough
+    public static int squareRoot(int a){
+        for(int i=0; i <= a; i++){
+            if(square(i)==a){
+                return i;
+            }
+        }
+        return -1; //means no squareroot found
+    }
+    //find the sin
+    //units radians
+    public static double sin(double a){
+        return Math.sin(a);
+    }
+    //find cosine
+    //units radians
+    public static double cosine(double a){
+        return Math.cos(a);
+    }
+    //find tangent
+    //units radians
+    public static double tan(double a){
+        return Math.tan(a);
+    }
+    //find factorial
+    public static int factorial(int n){
+        if(n<0){
+            throw new RuntimeException("cannot take factorial of a negative");
+        }
+        else if(n==1 || n==0){
+            return 1;
+        }
+        else{
+            return n * factorial(n-1);
+        }
+    }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -8,5 +8,15 @@ public class Main {
         System.out.println(Calculator.divide(a,b));
         System.out.println(Calculator.square(a));
 
+        int c = 25;
+        System.out.println("time for magic");
+        System.out.println(MagicCalculator.squareRoot(c));
+        System.out.println(MagicCalculator.sin(0));
+        System.out.println(MagicCalculator.cosine(0));
+        System.out.println(MagicCalculator.tan(180));
+        System.out.println(MagicCalculator.factorial(5));
+        System.out.println(MagicCalculator.factorial(0));
+        System.out.println(MagicCalculator.factorial(1));
+        System.out.println(MagicCalculator.factorial(2));
     }
 }
