f208c1b8232692b064bc7d99d80ddae4afee8f51
Jared Langson <langson.jared@gmail.com>
2020-11-16 20:53:10 -0500

    Finished Calculator

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,3 +1,23 @@
 public class Calculator {
-    
+    //add 2 ints
+    public static int add(int a, int b){
+        return a+b;
+    }
+    //subtract 2 ints
+    public static int subtract(int a, int b){
+        return a-b;
+    }
+    //multpy two ints
+    public static int multiply(int a, int b){
+        return a*b;
+    }
+    //divide two ints
+    public static int divide(int a, int b){
+        return a / b;
+    }
+    //square an int
+    public static int square(int a){
+        return multiply(a, a);
+    }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+        System.out.println(Calculator.add(a,b));
+        System.out.println(Calculator.subtract(a,b));
+        System.out.println(Calculator.multiply(a,b));
+        System.out.println(Calculator.divide(a,b));
+        System.out.println(Calculator.square(a));
 
     }
 }
